9c7532955cf85ce1fe1de7151eee54bb7b01a9c2
Aktilek20 <esenamanuulutilek@gmail.com>
2025-03-02 20:43:06 +0800

    project

--- js/swiper.js
+++ js/swiper.js
@@ -1,12 +1,52 @@
 
-new Swiper('.swiper-container', {
-  mousewheel: true,
+const swiper1 = new Swiper('#swiper-1', {
   freemode:true,
-  keyboard: {
-    enabled: true,
-  },
   pagination: {
-    el: ".pagination",
+    el: ".swiper-pagination-1",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+    485: {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    }
+  },
+});
+
+const swiper2 = new Swiper('#swiper-2', {
+  freemode:true,
+  pagination: {
+    el: ".swiper-pagination-2",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+    485: {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    }
+  },
+});
+
+const swiper3 = new Swiper('#swiper-3', {
+  freemode:true,
+  pagination: {
+    el: ".swiper-pagination-3",
     clickable: true,
   },
   breakpoints: {
